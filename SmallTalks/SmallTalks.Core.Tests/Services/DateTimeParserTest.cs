ac02337f92b99cbd04c4ee0be436f07b464104f8
Arthur Iperoyg Rodrigues Carvalho (Take.net) <arthurc@take.net>
2018-09-04 21:31:15 -0300

    add textc

---
+++ SmallTalks/SmallTalks.Core.Tests/Services/DateTimeParserTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using SmallTalks.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Takenet.Textc;
+
+namespace SmallTalks.Core.Tests.Services
+{
+    [TestFixture]
+    public class DateTimeParserTest
+    {
+
+        [TestCase("hoje")]
+        public void Simple_DateTimeParserTest(string input)
+        {
+            var context = new RequestContext();
+            var parser = new DateTimeParser();
+            var processor = parser.Build();
+            processor.ProcessAsync(input, context, CancellationToken.None).Wait();
+
+
+        }
+
+    }
+}
---
+++ SmallTalks/SmallTalks.Core/Services/DateTimeParser.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Takenet.Textc;
+using Takenet.Textc.Csdl;
+using Takenet.Textc.PreProcessors;
+using Takenet.Textc.Processors;
+
+namespace SmallTalks.Core.Services
+{
+    public class DateTimeParser
+    {
+        public TextProcessor Build()
+        {
+            var todaySyntax = CsdlParser.Parse("[today:LDWord(hoje,hj)]");
+            var tomorrowSyntax = CsdlParser.Parse("[tomorrow:LDWord?(amanha)]");
+            var yesterdaySyntax = CsdlParser.Parse("[yesterday:LDWord?(ontem)]");
+
+            MyDateTime myDateTime = new MyDateTime();
+
+            var todayCommandProcessor = new ReflectionCommandProcessor(
+                myDateTime,
+                nameof(myDateTime.Add),
+                true,
+                outputProcessor: null,
+                syntaxes: todaySyntax);
+            // 5. Register the the processors
+            var textProcessor = new TextProcessor();
+            textProcessor.CommandProcessors.Add(todayCommandProcessor);
+
+            // 6. Add some preprocessors to normalize the input text
+            textProcessor.TextPreprocessors.Add(new TextNormalizerPreprocessor());
+            textProcessor.TextPreprocessors.Add(new ToLowerCasePreprocessor());
+            return textProcessor;
+        }
+
+    }
+
+    public class MyDateTime
+    {
+        public Task<string> Add(string today, IRequestContext context)
+        {
+            return Task.FromResult(today);
+        }
+    }
+
+}
